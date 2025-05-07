2b855597f8d0c9cfe82cd3145a4425814f38d33c
Aryaveer Sharma <24csu306@ncuindia.edu>
2025-05-07 15:29:59 +0530

    Create Q18.cpp

---
+++ Q18.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void printReverse(int n) {
+    if(n == 0) return;
+    cout << n % 10 << " ";
+    printReverse(n / 10);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    printReverse(n);
+    return 0;
+}
